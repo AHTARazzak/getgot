1518d9bb870e4f5a133a1fb00fa8c63fd8b8aadb
ternlef11 <aliarazzak@hotmail.com>
2020-07-03 20:46:16 +0200

    new feelway but bad :(

--- feelwayscrapper/feelwayscrapper.py
+++ feelwayscrapper/feelwayscrapper.py
@@ -34,27 +34,27 @@ class Sel(unittest.TestCase):
         owd
         #1) include more brands if you want, spelling and composition is important ['Damir Doma']
         #CAN ONLY DO ONE BRAND AT A TIME
-        for brand in ['Prada']:
+        for brand in ['Louis Vuitton']:
             self.directoryname=brand.replace(" ","")
             os.chdir(owd)
             if not os.path.exists(self.directoryname):
                 os.makedirs(self.directoryname)
             os.chdir(self.directoryname)
             if not os.path.exists(self.directoryname+"catalogue.csv"):
-                append_list_as_row(self.directoryname+"catalogue.csv",["Brand", "ItemCode","URL","Title","Price(CHF)","Description","Time"])
-            self.branddirect = brand.replace(" ","%20").lower()
+                append_list_as_row(self.directoryname+"catalogue.csv",["Brand", "ItemCode","URL","Title","Price(WON)","Time"])
+            self.branddirect = brand.replace(" ","+").lower()
             options = webdriver.ChromeOptions()
             options.add_argument('--ignore-certificate-errors')
             options.add_argument('--incognito')
             options.add_argument('--headless')
             #2) Need to path to where ever your chrome is located,
             self.driver = webdriver.Chrome("/usr/local/share/chromedriver", options=options)
-            self.base_url = "https://www.ricardo.ch/de/s/"+self.branddirect+'?next_offset=59&page=1'
+            self.base_url = "https://www.feelway.com/list.php?f_key_goods="+self.branddirect+'&goods_page=1#self'
             self.verificationErrors = []
             self.accept_next_alert = True
 
     def test_sel(self):
-        folderpath='/home/a/Desktop/github/clothingscrapers/ricardoscrapper/'+self.directoryname+"/"
+        folderpath='/home/a/Desktop/github/clothingscrapers/feelwayscrapper/'+self.directoryname+"/"
         owd=os.getcwd()
         owd
         os.chdir(owd)
@@ -64,68 +64,55 @@ class Sel(unittest.TestCase):
         greaterhtml=[]
         itemlinks=[]
         pageitemsoup = BeautifulSoup(driver.page_source, 'lxml')
-        if "MuiFlatPagination-root jss598" in str(pageitemsoup):
-            pagesection=pageitemsoup.find_all('div',{'class' : 'MuiFlatPagination-root jss598'})[0]
-            onlypages=pagesection.find_all('a',{'href' : True})
-            thepage=(onlypages[-2].findAll(text=True)[0])
+        pagesection=pageitemsoup.find_all('div',{'style' : 'text-align: center'})
+        if "Total" in str(pagesection):
+            lastpage=(pagesection[0].getText().split()[-2])
         else:
-            thepage=int(thepage)
-        for page in range(1,int(thepage)+1):
-            self.base_url="https://www.ricardo.ch/de/s/"+self.branddirect+'?next_offset=59&page='+str(thepage)
+            lastpage=(pagesection[0].getText().split()[-1])
+        for page in range(1,int(lastpage)+1):
+            self.base_url="https://www.feelway.com/list.php?f_key_goods="+self.branddirect+'&goods_page='+str(page)+'#self'
             driver = self.driver
             driver.get(self.base_url)
+            WebDriverWait(driver, 1)
             pageitemsoup = BeautifulSoup(driver.page_source, 'lxml')
-            linktags=pageitemsoup.find_all('a',{'class' : 'MuiGrid-root link--2OHFZ MuiGrid-item MuiGrid-grid-xs-6 MuiGrid-grid-sm-4 MuiGrid-grid-md-3'})
-            for itematag in linktags:
-                itemlinks.append(itematag['href'])
+            linktagsdiv=pageitemsoup.find_all('div',{'class' : 'realtime-products'})[0]
+            thelinks=linktagsdiv.find_all('a',{'class' : 'newA'})
+            for itematag in thelinks:
+                itemlinks.append(itematag['data-url'])
         for itemurl in itemlinks:
-            spliturl=itemurl.split("/")
-            splithypehn=spliturl[-2].split('-')
-            itemcode=splithypehn[-1]
+            spliturl=itemurl.split("_")
+            splithtml=spliturl[-1].split('.')
+            itemcode=splithtml[-2]
             if not os.path.exists(folderpath+itemcode):
                 ax=0
                 descriptionis="N/A"
-                bx=0
-                cx=0
-                dx=0
-                ex=0
+
                 os.makedirs(folderpath+itemcode)
                 driver = self.driver
                 delay = 3
-                base_url = "https://www.ricardo.ch"+itemurl
+                base_url = "https://www.feelway.com/"+itemurl
                 print(base_url)
                 driver.get(base_url)
-                WebDriverWait(driver, 3)
+                WebDriverWait(driver, 2)
                 item_soup = BeautifulSoup(driver.page_source,'lxml')
-                titlesoup=item_soup.findAll('h1',{'class' : 'title--30fKd'})[0].text
-                if "jss170 valueprice--23qfn" in str(item_soup):
-                    pricesoup=item_soup.findAll('p',{'class' : 'jss170 valueprice--23qfn'})[0].text
-                else:
-                    pricesoup=item_soup.findAll('p',{'class' : 'jss170 value--2CfaM'})[0].text
-                thedescriptiongen=item_soup.findAll('div',{'class' : 'userDescription--1-bzj'})[0]
-                thedescription=thedescriptiongen.findAll('p')
-                if len(thedescription)>0:
-                    ax=1
-                    thedescriptionactual=thedescription[0].text
-                if ax==0:
-                    thedescriptionactual="N/A"
-                append_list_as_row(folderpath+self.directoryname+"catalogue.csv",[self.branddirect.replace("+"," "), itemcode, base_url,titlesoup,pricesoup,thedescriptionactual,datetime.datetime.now()])
-                imagearea=item_soup.findAll('div',{'role' : 'presentation'})[0]
+                titlesoup=item_soup.findAll('div',{'class' : 'kbo-product-title'})[0].getText().strip()
+                pricesoup=item_soup.findAll('p',{'class' : 'discount'})[0]
+                theprice=pricesoup.findAll('em')[0].getText().strip()
+                append_list_as_row(folderpath+self.directoryname+"catalogue.csv",[self.branddirect.replace("+"," "), itemcode, base_url,titlesoup,theprice,datetime.datetime.now()])
+                imagearea=item_soup.findAll('div',{'class' : 'kbo-product-image-wrapper'})[0]
                 theimages=imagearea.findAll('img')
                 imageurls=[]
                 for eachimage in theimages:
-                    imageurls.append(str(eachimage['src']))
+                    imageurls.append(str(eachimage['src'][2:]))
                 for clothingimage in imageurls:
+                    print(clothingimage)
                     splitimage=clothingimage.split('/')
-                    urllib.request.urlretrieve(str(clothingimage), str(splitimage[-2]))
-                    shutil.move(os.getcwd()+'/'+splitimage[-2], os.getcwd()+'/'+itemcode+'/'+splitimage[-2])
-                    ax=1
-                    bx=1
-                    cx=1
-                    dx=1
-                    ex=1
-                    fx=1
-                    gx=1
+                    try:
+                        urllib.request.urlretrieve('https://'+str(clothingimage), str(splitimage[-1]))
+                        shutil.move(os.getcwd()+'/'+splitimage[-1], os.getcwd()+'/'+itemcode+'/'+splitimage[-1])
+                    except UnicodeEncodeError:
+                        pass
+
 
 if __name__ == "__main__":
     unittest.main()
